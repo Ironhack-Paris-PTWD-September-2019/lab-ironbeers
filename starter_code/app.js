0d1137d5432bc7910e17db59a791eb619b948316
Viviajera <vivianajimenezh@gmail.com>
2019-11-23 17:58:27 +0100

    First commit Viviana et Jenny

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,17 +5,34 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+const randomBeer = punkAPI.getRandom()
+
+//randomBeer.then(beer => {
+  //alert(beer[0].name)
+//})
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
-
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beersData => {
+      console.log(beersData);
+      res.render('beers', {
+        beersData: beersData
+      });
+    })
+    .catch(error => {
+      console.log('oops');
+      console.log(error)
+    })
+});
 
 
 app.listen(3000);
