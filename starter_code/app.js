ed85d0937a9463ef9a83601c5b092347bdd330c9
Tracy Bonjour <tracybonjour@MacBook-Air-de-Tracy.local>
2019-11-23 18:28:15 +0100

    Tracy

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,12 +8,37 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      res.render('beers', {
+        beers: beers
+      });
+      
+    })
+    
+});
+
+app.get('/randomBeers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('randomBeers', {
+      beers: [0]
+    });
+
+  })
+  
+  
 });
 
 
